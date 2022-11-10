abc8e672fea8966a554fe61b173e38887331a3c4
feranmi@2002 <oluwaoyehunferanmi@gmail.com>
2022-11-10 01:42:08 +0100

    Initial commit

---
+++ app/src/main/java/com/faithdeveloper/explore/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.faithdeveloper.explore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
