68addfc18c26eafc8a347885fe39789a254ff674
feranmi2002 <oluwaoyehunferanmi@gmail.com>
2023-01-16 21:58:23 +0100

    Improved image loading by setting up infinite retry for Glide when load of image fails

--- app/src/main/java/com/faithdeveloper/explore/adapters/ImagesAdapter.kt
+++ app/src/main/java/com/faithdeveloper/explore/adapters/ImagesAdapter.kt
@@ -1,6 +1,5 @@
 package com.faithdeveloper.explore.adapters
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
@@ -9,64 +8,40 @@ import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.faithdeveloper.explore.databinding.PicturesItemBinding
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import com.google.android.material.textview.MaterialTextView
 
-class ImagesAdapter(private val images: List<String>, val save: (bitmap:Bitmap) -> Unit) :
+class ImagesAdapter(private val images: List<String>, val save: (bitmap: Bitmap) -> Unit) :
     RecyclerView.Adapter<ImagesAdapter.ImageViewHolder>() {
 
-    inner class ImageViewHolder(val binding: PicturesItemBinding) :
+    inner class ImageViewHolder(private val binding: PicturesItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         private var mUrl: String? = null
 
         init {
-            binding.retry.setOnClickListener {
-                bind(mUrl)
-            }
-
             binding.save.setOnClickListener {
                 save.invoke(binding.picture.drawable.toBitmap())
             }
+
         }
 
         fun bind(url: String?) {
             mUrl = url
             binding.save.isVisible = false
-            binding.retry.isVisible = false
             binding.progressCircular.isVisible = true
-            Glide.with(itemView.context)
-                .load(url)
-                .addListener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        binding.progressCircular.isVisible = false
-                        binding.retry.isVisible = true
-                        binding.save.isVisible = false
-                        return false
-                    }
+            binding.picture.loadImage(
+                url,
+                binding.progressCircular,
+                binding.save
+            )
 
-                    override fun onResourceReady(
-                        resource: Drawable?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        dataSource: DataSource?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        binding.progressCircular.isVisible = false
-                        binding.retry.isVisible = false
-                        binding.save.isVisible = true
-                        return false
-                    }
-
-                })
-                .into(binding.picture)
         }
     }
 
@@ -85,4 +60,48 @@ class ImagesAdapter(private val images: List<String>, val save: (bitmap:Bitmap) 
     }
 
     override fun getItemCount() = images.size
+
+    private fun ShapeableImageView.loadImage(
+        resource: Any?,
+        progressCircular: CircularProgressIndicator,
+        save: MaterialTextView
+    ) {
+        getGlideImage(resource, progressCircular, save).into(this)
+    }
+
+    private fun ShapeableImageView.getGlideImage(
+        resource: Any?,
+        progressCircular: CircularProgressIndicator,
+
+        save: MaterialTextView
+    ): RequestBuilder<Drawable>{
+        return Glide.with(this.context)
+            .load(resource)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    this@getGlideImage.post {
+                        loadImage(resource, progressCircular, save)
+                    }
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressCircular.isVisible = false
+                    save.isVisible = true
+                    return false
+                }
+
+            })
+    }
 }
--- app/src/main/java/com/faithdeveloper/explore/paging/ExplorePager.kt
+++ app/src/main/java/com/faithdeveloper/explore/paging/ExplorePager.kt
@@ -1,13 +1,23 @@
 package com.faithdeveloper.explore.paging
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.faithdeveloper.explore.util.ITEM_COMPARATOR
 import com.faithdeveloper.explore.databinding.CountriesListItemBinding
 import com.faithdeveloper.explore.models.Country
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import com.google.android.material.textview.MaterialTextView
 
 class ExplorePager(private val onClick: (country: Country) -> Unit) :
     PagingDataAdapter<Country, ExplorePager.ExploreViewHolder>(ITEM_COMPARATOR) {
@@ -25,12 +35,10 @@ class ExplorePager(private val onClick: (country: Country) -> Unit) :
         fun bind(item: Country?) {
             mItem = item
             binding.country.text = item?.name?.official
-            if (item?.capital?.isNotEmpty() == true){
+            if (item?.capital?.isNotEmpty() == true) {
                 binding.capital.text = item?.capital?.first()
             }
-            Glide.with(itemView.context)
-                .load(item?.flags?.png)
-                .into(binding.flag)
+            binding.flag.loadImage(item?.flags?.png)
         }
     }
 
@@ -46,5 +54,42 @@ class ExplorePager(private val onClick: (country: Country) -> Unit) :
                 false
             )
         )
+    }
+
+    private fun ShapeableImageView.loadImage(
+        resource: Any?
+    ) {
+        getGlideImage(resource).into(this)
+    }
+
+    private fun ShapeableImageView.getGlideImage(
+        resource: Any?
+    ): RequestBuilder<Drawable> {
+        return Glide.with(this.context)
+            .load(resource)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    this@getGlideImage.post {
+                        loadImage(resource)
+                    }
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+            })
     }
 }
